22bf38364e2dfcbec867a78d48b477ced2b9e6d1
Gunjan Jain <jain10gunjan@gmail.com>
2024-07-17 16:45:55 +0530

    Fixed Front Issues.

--- src/mockdata/course.js
+++ src/mockdata/course.js
@@ -8,7 +8,7 @@ export const courseData = [
       "The Certificate course on Competition Law is meticulously designed to provide participants with a comprehensive understanding of competition law principles and their application in various sectors.",
     course_fee: {
       original_price: "₹10,000",
-      current_price: "₹2999",
+      current_price: "₹3000",
     },
     coursename: "Certificate Course on Competition Law",
     color: "gray-50",
@@ -21,7 +21,7 @@ export const courseData = [
       "The Certificate Course in Comparative Criminal Law is a specialized program designed to provide participants with a thorough understanding of the evolution of Indian criminal law by comparing the old Indian Criminal Law with the newly formed legislation.",
     course_fee: {
       original_price: "₹11,000",
-      current_price: "₹2999",
+      current_price: "₹3000",
     },
     coursename: "Certificate Course in Comparative Criminal Law",
     color: "gray-900",
@@ -30,14 +30,14 @@ export const courseData = [
   {
     duration: "4 months",
     batches: "starting periodically",
-    link: "/courses/6676563d72f4b13ae10bc0de",
+    link: "/courses/66977de379aac3bbb3f33441",
     description:
-      "The Certificate Course in Comparative Criminal Law is a specialized program designed to provide participants with a thorough understanding of the evolution of Indian criminal law by comparing the old Indian Criminal Law with the newly formed legislation.",
+      "This comprehensive certificate course, led by Adv. Nitesh Dwivedi, focuses on equipping students with the essential skills and knowledge required for effective legal drafting and conveyancing.",
     course_fee: {
       original_price: "₹11,000",
-      current_price: "₹2999",
+      current_price: "₹3000",
     },
-    coursename: "Certificate Course in Comparative Criminal Law",
+    coursename: "Certificate Course in Drafting and Conveyancing",
     color: "gray-900",
     textColor: "white",
   },
