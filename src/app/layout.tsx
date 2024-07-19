ed71a7b73b521437c7ca2719e42d07d52e89e266
ambitiousacademy <ambitiousacademyadp@gmail.com>
2024-07-19 12:31:57 +0530

    Revert to Original

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,19 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-  <ClerkProvider>
-      <html lang='en'>
-        <head>
-          <title>{metadata.title}</title>
-          <meta name="description" content={metadata.description} />
-          <meta name="robots" content="noindex, nofollow">
-             </head>
-           <body>
-          <Navbar />
-          {children}
-          <Footer />
-          <Script src="https://checkout.razorpay.com/v1/checkout.js" />
-        </body>
+ <ClerkProvider>
+    <html lang='en'>
+    <Script src="https://checkout.razorpay.com/v1/checkout.js" />
+      <body>
+        <Navbar/>
+        {children}
+        <Footer/>
+      </body>
     </html>
   </ClerkProvider>
   );
