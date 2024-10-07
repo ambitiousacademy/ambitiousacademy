4490136f8a0b8c3bb316544c8ef68c33e056e79a
ambitiousacademy <ambitiousacademyadp@gmail.com>
2024-10-08 00:14:50 +0530

    Update Coursecarousel.tsx

--- src/components/Coursecarousel.tsx
+++ src/components/Coursecarousel.tsx
@@ -3,7 +3,7 @@ import '/src/app/globals.css';
 
 const Carouselcarousel: React.FC = () => {
   const slides = [
-    { src: "./photos/CertificateCourseonDraftingandConveyance.jpg.jpg", link:"/courses/66977de379aac3bbb3f33441" },
+    { src: "./photos/CertificateCourseonDraftingandConveyance.jpg", link:"/courses/66977de379aac3bbb3f33441" },
     { src: "./photos/CertificateCourseonComparativeCriminalLaw.jpg", link:"/courses/6676563d72f4b13ae10bc0de" },
     { src: "./photos/CertificateCourseonEnviromentalLaw.jpg", link:"/courses/66a534c9a388f8e38c30c5c6" },
     { src: "./photos/CertificateCourseonCompetitionLaw.jpg", link: "/courses/667546bf72f4b13ae138e3a1" },
